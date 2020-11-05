9b97553775c113f01a40be22bf501e311557f0d9
Phillip Zeba <phillipzeba@Phillips-MacBook-Air.local>
2020-11-05 18:00:32 +0100

    rebrand from weather app to something else

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import requests
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route('/')
+def index():
+    return render_template('index.html')
