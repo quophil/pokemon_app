fcfe80183e4099653bb0e75c24e4aa660ad4bffe
Phillip Zeba <phillipzeba@Phillips-MacBook-Air.local>
2020-12-13 01:51:39 +0100

    added prototype of result upon search

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('layout.html')
 
 @app.route('/', methods=['POST'])
 def pokemon_search_result():
-    pokemon_name = request.form['pokemon_name']
-    return pokemon_name
+    pokemon_name_bleh = request.form['pokemon_name']
+    return render_template('pk_bruv.html', pokemon_name = pokemon_name_bleh)
