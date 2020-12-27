b749cf11bc297261783a153d1fe26322d9a0710b
Phillip Zeba <phillip.zeba@gmx.at>
2020-12-27 16:08:04 +0100

    made the base idea function

--- app.py
+++ app.py
@@ -13,23 +13,28 @@ def index():
 @app.route('/', methods=['POST'])
 def pokemon_search_result():
     pokemon_entry = request.form['pokemon_entry']
-    r = requests.get(url.format(pokemon_entry.lower())).json()
-    pokemon = {
-        'pokemon_name': r['species']['name'],
-        'pokemon_index': r['game_indices']['19']['game_index'],
-        'pokemon_type1': r['types']['0']['type']['name'],
-        'pokemon_type2': 'No Type 2',
-        'pokemon_weight': r['types']['weight'],
-        'pokemon_height': r['game_indices']['height']
-    }
+    try:
+        r = requests.get(url.format(pokemon_entry.lower())).json()
+        pokemon = {
+            'pokemon_name': r['species']['name'],
+            'pokemon_sprite': r['sprites']['front_default'],
+            'pokemon_index': r['game_indices'][0]['game_index'],
+            'pokemon_type1': r['types'][0]['type']['name'],
+            'pokemon_type2': 'No Type 2',
+            'pokemon_weight': r['weight'],
+            'pokemon_height': r['height']
+        }
 
-    if len(r['types']) >= 3:
-        pokemon['pokemon_type2'] = r['types']['1']['type']['name']
-    return render_template('pokemon_template.html', 
-    pokemon_name=pokemon['pokemon_name'], 
-    pokemon_index=pokemon['pokemon_index'],
-    pokemon_type1=pokemon['pokemon_type1'],
-    pokemon_type2=pokemon['pokemon_type2'],
-    pokemon_weight=pokemon['pokemon_weight'],
-    pokemon_height=pokemon['pokemon_height']
-    )
+        if len(r['types']) >= 3:
+            pokemon['pokemon_type2'] = r['types'][1]['type']['name']
+        return render_template('pokemon_template.html', 
+        pokemon_name=pokemon['pokemon_name'],
+        pokemon_sprite=pokemon['pokemon_sprite'], 
+        pokemon_index=pokemon['pokemon_index'],
+        pokemon_type1=pokemon['pokemon_type1'],
+        pokemon_type2=pokemon['pokemon_type2'],
+        pokemon_weight=pokemon['pokemon_weight'],
+        pokemon_height=pokemon['pokemon_height']
+        )
+    except:
+        return render_template('layout.html')
